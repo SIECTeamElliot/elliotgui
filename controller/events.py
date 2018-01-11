c229d783b19bcec159d3041733b82541bcc129fc
Johan Medrano <jmsnowrider@gmail.com>
2018-01-11 08:50:58 +0100

    ajout event avec log

--- controller/events.py
+++ controller/events.py
@@ -2,36 +2,34 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 class Event():
-    def __init__(self, event_key, callback_trig_on=None, callback_trig_off=None, has_value=False, default_val=0):
+    def __init__(self, event_key, callback_trig=None, has_value=False, default_val=0):
         if has_value:
             self.value = default_val
         self.has_value = has_value
         self.enable = False
         self.event_key = event_key
-        self.callback_trig_on = callback_trig_on
-        self.callback_trig_off = callback_trig_off
+        self.callback_trig = callback_trig
 
     def trig_on(self):
         self.enable = True
-        if callable(self.callback_trig_on):
-            self.callback_trig_on()
+        if callable(self.callback_trig):
+            self.callback_trig("on")
 
     def trig_off(self):
         self.enable = False
-        if callable(self.callback_trig_off):
-            self.callback_trig_off()
+        if callable(self.callback_trig):
+            self.callback_trig("off")
 
 class Callback:
     def __init__(self, event, logger):
         self.event = event
         self.logger = logger
 
-    def __call__(self):
-        print(self.event)
-        self.logger.info(self.event)
+    def __call__(self, value):
+        self.logger.info(self.event + " " + value)
 
 class EventRegistry():
-    def __init__(self, filename="./communication_file.txt"):
+    def __init__(self, filename="../communication_file.txt"):
         self.handler = RotatingFileHandler(filename, mode='a', maxBytes=4*10 , backupCount=0, encoding=None, delay=0)
         self.handler.setLevel(logging.INFO)
 
